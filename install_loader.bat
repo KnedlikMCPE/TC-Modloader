efac44d614a0ef96c39c83395327042b20e35640
KnedlikMCPE <knedlik@trainmazeland.com>
2022-07-11 14:33:15 +0200

    Update install_loader.bat

--- install_loader.bat
+++ install_loader.bat
@@ -3,6 +3,7 @@ del "extracted/main_scripts/init.gdc"
 del "extracted/main_scripts/init.gd.remap"
 xcopy init.gd extracted/main_scripts/
 godotpcktool "Turing Complete.pck" -a a extracted --remove-prefix extracted
-del /S /Q extracted
-del "../Turing Complete.pck"
-xcopy "Turing Complete.pck" ../
+rmdir /S /Q extracted
+xcopy "Turing Complete.pck" "../" /y
+del "Turing Complete.pck"
+pause

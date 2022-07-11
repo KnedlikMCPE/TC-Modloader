7bb8fa5fda5b067dccaf82dffe71625d13cdb820
KnedlikMCPE <knedlik@trainmazeland.com>
2022-07-11 17:26:54 +0200

    Almost done

---
+++ Loader/TCModLoader/Main.gd
@@ -0,0 +1,33 @@
+extends Node
+
+func _ready():
+	print("Starting modloader")
+	
+	var files = listDir("user://")
+	
+	for file in files:
+		print("loading resource pack: " + file)
+		ProjectSettings.load_resource_pack("user://" + file)
+		var modMain = load("res://" + file.split(".")[0] + "/Main.gd")
+		var modNode = Node.new()
+		modNode.set_script(modMain)
+		get_tree().get_root().call_deferred("add_child", modNode)
+
+	print("Loaded!")
+
+func listDir(tgtDir: String):
+	var files = []
+	var dir = Directory.new()
+	dir.open(tgtDir)
+	dir.list_dir_begin()
+	while true:
+		var file = dir.get_next()
+		if file == "":
+			print("Breaking")
+			break
+		if file.ends_with(".pck"):
+			print("appending file")
+			files.append(file)
+			
+	dir.list_dir_end()
+	return files
--- install_loader.bat
+++ install_loader.bat
@@ -1,7 +1,7 @@
 godotpcktool "../Turing Complete.pck" -a e -o extracted
-del "extracted/main_scripts/init.gdc"
-del "extracted/main_scripts/init.gd.remap"
-xcopy init.gd extracted/main_scripts/
+del /f "extracted\main_scripts\init.gdc"
+del /f "extracted\main_scripts\init.gd.remap"
+xcopy "init.gd" "extracted/main_scripts/" /y
 godotpcktool "Turing Complete.pck" -a a extracted --remove-prefix extracted
 rmdir /S /Q extracted
 xcopy "Turing Complete.pck" "../" /y

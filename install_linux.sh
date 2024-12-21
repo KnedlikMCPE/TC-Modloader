3117f90dfa67289d3ebeb43494f48ece4096d7ad
Amber Koudelková <amber@saygex.gay>
2024-12-21 22:59:19 +0100

    Fix

--- install_linux.sh
+++ install_linux.sh
@@ -1,9 +1,9 @@
 ./godotpcktool "../Turing Complete.pck" -a e -o extracted
 rm "extracted\main_scripts\init.gdc"
 rm "extracted\main_scripts\init.gd.remap"
-cp "init.gd" "extracted/main_scripts/" /y
+cp "init.gd" "extracted/main_scripts/"
 ./godotpcktool "Turing Complete.pck" -a a extracted --remove-prefix extracted
 rm -r extracted
-cp "Turing Complete.pck" "../" /y
+cp "Turing Complete.pck" "../"
 rm "Turing Complete.pck"
 cp "TCModLoader.pck" "../"
